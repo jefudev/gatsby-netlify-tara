8fa554b03006ccb5f66f18a7f853471f29f68b37
Jeff Lau <jefl2109@gmail.com>
2019-09-04 23:16:03 -0600

    installed semantic ui react with theming

---
+++ src/components/footer.js
@@ -0,0 +1,7 @@
+import React from "react"
+const Footer = () => (
+    <footer id="footer">
+    </footer>
+)
+
+export default Footer
---
+++ src/components/maincontainer.js
@@ -0,0 +1,9 @@
+import React from "react"
+import Image from "../components/image"
+
+const MainContainer = () => (
+  <div>
+  </div>  
+)
+
+export default MainContainer
