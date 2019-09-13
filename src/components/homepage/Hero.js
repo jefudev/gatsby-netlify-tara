135466178964218750c3d0794076582607661f8d
Jeff Lau <jefl2109@gmail.com>
2019-09-13 12:31:21 -0600

    removed typewriter effect

--- src/components/homepage/Hero.js
+++ src/components/homepage/Hero.js
@@ -1,50 +1,17 @@
-import React from "react"
-import {
-  Image,
-  Container,
-} from 'semantic-ui-react'
+import React, {Component} from "react"
 import 'semantic-ui-less/semantic.less'
-import Typewriter from 'typewriter-effect';
 
-import heroImage from '../../images/homepage-hero.jpg'
-const Hero = () => (
-  <div className='hero__img--container'>
-    <Typewriter
-      options={{
-        autoStart: true,
-        loop: false,
-        delay: 75,
-      }}
-      onInit={(typewriter) => {
-        typewriter.typeString('')
-        .pauseFor(2000)
-        typewriter.typeString('<span class="typewriter__name">Tara<br>Richter</span><br>')
-        .pauseFor(1000)
-        typewriter.typeString('<strong>Author</strong>')
-        .pauseFor(1000)
-        .deleteChars(6)
-        typewriter.typeString('<strong>Publisher</strong>')
-        .pauseFor(1000)
-        .deleteChars(9)
-        typewriter.typeString('<strong>Writing Coach</strong>')
-        .pauseFor(1000)
-        .deleteChars(13)
-        typewriter.typeString('<strong>Philanthropist</strong>')
-        .pauseFor(1000)
-        .deleteChars(14)
-        typewriter.typeString('<strong>Entrepreneur</strong>')
-        .pauseFor(1000)
-        .deleteChars(12)
-        typewriter.typeString('<strong>Superwoman</strong>')
-        .pauseFor(1000)
-        .start();
-      }}
-    />
-    <div id='hero__img'></div>
-  </div>
-)
+class Hero extends Component {
 
-
-
+  render() {
+    return (
+      <div className='hero__img--container'>
+        <span class="typewriter__name">Tara<br />Richter</span><br/>
+        Author, Publisher, Writing Coach, and Philanthropist
+        <div id='hero__img'></div>
+      </div>
+    )
+  }
+}
 
 export default Hero
