3fffe4197c06d999c8dfbfe3e17758c2285f415b
Jeff Lau <jefl2109@gmail.com>
2019-09-15 21:30:53 -0600

    updated contact form

--- src/components/homepage/ContactSegment.js
+++ src/components/homepage/ContactSegment.js
@@ -5,33 +5,59 @@ import {
 } from 'semantic-ui-react'
 import 'semantic-ui-less/semantic.less'
 import '../../styles/main.css'
+import { navigate } from 'gatsby-link'
 
-const ContactSegment = () => (
-  <Container className='home__contact-form'>
-    <Form name="contact" method="POST" data-netlify="true">
+
+function encode(data) {
+  return Object.keys(data)
+    .map((key) => encodeURIComponent(key) + '=' + encodeURIComponent(data[key]))
+    .join('&')
+}
+
+export default function ContactSegment() {
+  const [state, setState] = React.useState({})
+
+  const handleChange = (e) => {
+    setState({ ...state, [e.target.name]: e.target.value })
+  }
+
+  const handleSubmit = (e) => {
+    e.preventDefault()
+    const form = e.target
+    fetch('/', {
+      method: 'POST',
+      headers: { 'Content-Type': 'application/x-www-form-urlencoded' },
+      body: encode({
+        'form-name': form.getAttribute('name'),
+        ...state,
+      }),
+    })
+      .then(() => navigate(form.getAttribute('action')))
+      .catch((error) => alert(error))
+  }
+
+  return (
+    <Container className='home__contact-form'>
+    <Form name="contact"
+        method="post"
+        action="/thanks/"
+        data-netlify="true"
+        data-netlify-honeypot="bot-field"
+        onSubmit={handleSubmit}>
+      <input type="hidden" name="form-name" value="contact" />
+      <p hidden>
+        <label>
+          Don’t fill this out: <input name="bot-field" onChange={handleChange} />
+        </label>
+      </p>
       <Form.Group widths='equal'>
-        <Form.Input fluid label='Name' placeholder='Name' type="text" name='name'/>
-        <Form.Input fluid label='Email' placeholder='Email' type="email" name='email'/>
+        <Form.Input fluid label='Name' placeholder='Name' type="text" name='name' onChange={handleChange} />
+        <Form.Input fluid label='Email' placeholder='Email' type="email" name='email' onChange={handleChange} />
       </Form.Group>
-      <Form.TextArea label='Message' placeholder='I look forward to connecting with you!' name='message' />
+      <Form.TextArea label='Message' placeholder='I look forward to connecting with you!' name='message' onChange={handleChange} />
       <Form.Checkbox label='I agree to the Terms and Conditions' />
       <Form.Button type='submit'>Submit</Form.Button>
     </Form>
-    <form name="contact" method="POST" data-netlify="true">
-  <p>
-    <label>Your Name: <input type="text" name="name" /></label>
-  </p>
-  <p>
-    <label>Your Email: <input type="email" name="email" /></label>
-  </p>
-  <p>
-    <label>Message: <textarea name="message"></textarea></label>
-  </p>
-  <p>
-    <button type="submit">Send</button>
-  </p>
-</form>
   </Container>
-)
-
-export default ContactSegment
+  )
+}
---
+++ src/pages/thanks.js
@@ -0,0 +1,16 @@
+import React from "react"
+import { Link } from "gatsby"
+import '../styles/main.css'
+
+import SEOComponent from "../components/SEOComponent"
+import HomepageLayout from "../layouts/HomepageLayout"
+
+const Thanks = (props) => {
+    return(
+        <HomepageLayout>
+            Thank You! <br/>
+            I get back to you as soon as I can!
+        </HomepageLayout>
+)}
+
+export default Thanks
